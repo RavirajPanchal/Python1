122c4f918ad7b3d19c270ce46e4bca128be35748
RavirajPanchal <117341418+RavirajPanchal@users.noreply.github.com>
2022-11-04 13:32:32 +0530

    Add files via upload

---
+++ Day1/PythonD1.py
@@ -0,0 +1,48 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+print("hello world")
+hello world
+
+cource='hello'
+print(cource)
+hello
+
+
+full_name='john smith'
+age=20
+is_new=True
+print(new)
+Traceback (most recent call last):
+  File "<pyshell#10>", line 1, in <module>
+    print(new)
+NameError: name 'new' is not defined
+print(fuill_name)
+Traceback (most recent call last):
+  File "<pyshell#11>", line 1, in <module>
+    print(fuill_name)
+NameError: name 'fuill_name' is not defined. Did you mean: 'full_name'?
+
+
+
+name=input('what is your name?')
+what is your name?Ravi
+print('hii' + name)
+hiiRavi
+
+
+fav_color=('what is ur fav color')
+fav_color=input('what is ur fav color')
+what is ur fav color blue
+print(name + 'likes' + fav_color)
+Ravilikes blue
+
+
+
+birth_year=input('enter ur birth year')
+enter ur birth year 1998
+age=2022-int(birth_year)
+print(age)
+24
+
+----------------
